4f3f071431757f5ac21550e84894102beb934cc7
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2016-01-24 23:47:08 -0200

    Add Route.

---
+++ lib/rack_router/route.rb
@@ -0,0 +1,121 @@
+class Route
+  attr_reader :method,
+              :path,
+              :matchers_chain
+
+  def initialize(args)
+    @method = args[:method]
+    @path = args[:path]
+    @matchers_chain = build_chain(chain_objs)
+  end
+
+  def matches_request?(request)
+    matchers_chain.run_chain(request)
+  end
+
+  private
+
+  def build_chain(objs)
+    objs[0..-2].each_with_index do |obj, index|
+      obj.next_obj = objs[index+1]
+    end
+
+    objs.first
+  end
+
+  def chain_objs
+    [
+      path_matcher,
+      MethodMatcher.new(method)
+    ]
+  end
+
+  def path_matcher
+    if path.include?(':')
+      ParametrizedPathMatcher.new(path)
+    else
+      ExactPathMatcher.new(path)
+    end
+  end
+
+
+
+  module MatcherChainOfResponsibility
+    attr_accessor :next_obj
+
+    def run_chain(request)
+      matches_request?(request) && call_next(request)
+    end
+
+    def call_next(request)
+      if next_obj.nil?
+        true
+      else
+        next_obj.matches_request?(request)
+      end
+    end
+  end
+
+  class ExactPathMatcher
+    include MatcherChainOfResponsibility
+
+    def initialize(path)
+      @path = path
+    end
+
+    def matches_request?(request)
+      prepare(request.path) == @path
+    end
+
+    private
+
+    def prepare(path)
+      if path[-1] == '/'
+        path[0..-2] 
+      else
+        path
+      end
+    end
+  end
+
+  class ParametrizedPathMatcher
+    include MatcherChainOfResponsibility
+
+    PARAMS_FINDER_REGEXP = /(:[^\/]+)/.freeze
+    PARAMS_REPLACEMENT_REGEXP = '[^\/]+'.freeze
+
+    def initialize(path)
+      @path_regexp = Regexp.new(build_path_regexp_string(path))
+    end
+
+    def matches_request?(request)
+      @path_regexp.match(prepare(request.path))
+    end
+
+    private
+
+    def prepare(path)
+      if path[-1] == '/'
+        path[0..-2]
+      else
+        path
+      end
+    end
+
+    def build_path_regexp_string(path)
+      '^' + path.sub(PARAMS_FINDER_REGEXP, PARAMS_REPLACEMENT_REGEXP) + '$'
+    end
+  end
+
+  class MethodMatcher
+    include MatcherChainOfResponsibility
+
+    def initialize(method)
+      @method = method
+    end
+
+    def matches_request?(request)
+      request.method == @method
+    end
+  end
+end
---
+++ test/route_test.rb
@@ -0,0 +1,134 @@
+require 'test_helper'
+require 'roles/route_matcher'
+require 'rack_router/route'
+
+class RequestDouble
+  def initialize(args = {})
+    @method = args[:method]
+    @path = args[:path]
+  end
+
+  def method
+    @method ||= :get
+  end
+
+  def path
+    @path ||= '/test/path'
+  end
+end
+
+class TestRoute < MiniTest::Unit::TestCase
+  include RouteMatcherTest
+
+  def setup
+    @sut = Route.new(
+      method: :get,
+      path: '/test/path'
+    )
+  end
+
+  def test_it_matches_with_exact_path
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/path'
+    )
+
+    matches = route.matches_request?(request)
+
+    assert matches
+  end
+
+  def test_it_matches_with_exact_path_and_trailing_slash
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path/'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/path'
+    )
+
+    matches = route.matches_request?(request)
+
+    assert matches
+  end
+
+  def test_it_matches_a_route_with_params
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/:param'
+    )
+
+    matches = route.matches_request?(request)
+    
+    assert matches
+  end
+
+  def test_it_matches_a_route_with_trailing_slash
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path/'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/:param'
+    )
+
+    matches = route.matches_request?(request)
+    
+    assert matches
+  end
+
+  def test_it_doesnt_match_a_path_with_extra_trailing_segments
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path/extra'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/:param'
+    )
+
+    matches = route.matches_request?(request)
+    
+    refute matches
+  end
+
+  def test_it_doesnt_match_a_path_with_extra_leading_segments
+    request = RequestDouble.new(
+      method: :get,
+      path: '/leading/test/path'
+    )
+    route = Route.new(
+      method: :get,
+      path: '/test/:param'
+    )
+
+    matches = route.matches_request?(request)
+    
+    refute matches
+  end
+
+  def test_it_doesnt_match_a_path_with_different_method
+    request = RequestDouble.new(
+      method: :get,
+      path: '/test/path'
+    )
+    route = Route.new(
+      method: :post,
+      path: '/test/path'
+    )
+
+    matches = route.matches_request?(request)
+    
+    refute matches
+  end
+end
